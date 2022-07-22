5604ccc198e52304e40d7614310c736d4f19b7ed
Muhammed Fatih Öztel <74593165+muhammed-oztel@users.noreply.github.com>
2022-07-22 11:39:43 +0300

    Decrementer added

--- react-todo/src/samples/HookExample.js
+++ react-todo/src/samples/HookExample.js
@@ -11,12 +11,20 @@ export default function HookExample() {
 
   }
 
+  const decrementCounter = () => {
+    setCount(count - 1);
+  }
+
   return (
     <div>
-      <p>You clicked {count} times</p>
+      <p>Counter value is {count}</p>
       <button onClick= {incrementCounter}>
         Increment
       </button>
+      <button onClick= {decrementCounter}>
+        Decrement
+        </button>
+
     </div>
   );
 }
